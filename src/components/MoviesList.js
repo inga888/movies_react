fb70e04eba4317978c7e813b85f81dd17bdd39c6
Inga Zhamsoeva <injoyme@gmail.com>
2022-03-23 00:34:41 +0300

    final release

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,12 +1,13 @@
 import Movie from './Movie';
 
 const MoviesList = (props) => {
-  const {movies} = props;
+  const {movies = []} = props;
   return (
     <div className="movies">
-      {movies.map((movie) => (
+      {movies.length ? movies.map((movie) => (
         <Movie key={movie.imdbID} {...movie}/> //используем спредоператором ключи объекта муви в пропсах
-      ))}
+      )) : <h4>Nothing was found</h4>
+    }
     </div>
   );
 }
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,31 +3,34 @@ import MoviesList from '../components/MoviesList';
 import Preloader from '../components/Preloader';
 import Search from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
   state = {
     movies: [],
+    loading: true,
   }
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=cd976b00&s=matrix')
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
     .then(response => response.json())
-    .then(data => this.setState({ movies: data.Search}))
+    .then(data => this.setState({ movies: data.Search, loading: false}))
   }
   searchMovies = (str, type = "all") => {
-    fetch(`http://www.omdbapi.com/?apikey=cd976b00&s=${str}${type !== "all" ? `&type=${type}` : ''}`)
+    this.setState({loading: true})
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== "all" ? `&type=${type}` : ''}`)
     .then(response => response.json())
-    .then(data => this.setState({ movies: data.Search}))
+    .then(data => this.setState({ movies: data.Search, loading: false}))
 
 
   }
   render(){
-    const {movies} = this.state;
+    const {movies, loading} = this.state;
     return (
       <main className="container content">
         <Search searchMovies={this.searchMovies}/>
         {
-          movies.length ? (
-          <MoviesList movies={this.state.movies}/>
-          ) : <Preloader />
+          loading ? <Preloader />
+          : (<MoviesList movies={movies}/>)
         }
 
       </main>
